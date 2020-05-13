74f449fab89a089f06ab3cc21103b7d6595c46d8
Habeebullah Agbaje <agbaj@Habeeb-PC.beeb>
2020-05-13 04:13:40 +0100

    initial commit

---
+++ precache-manifest.272a4621cd6b6b8212da60f909208d89.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe66327860dc0a453b56fc868936bfd1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0340f115cbb1695bae15",
+    "url": "/static/css/2.f93178fb.chunk.css"
+  },
+  {
+    "revision": "3bf9b4b64fe2376c47d5",
+    "url": "/static/css/main.2ac4ad28.chunk.css"
+  },
+  {
+    "revision": "0340f115cbb1695bae15",
+    "url": "/static/js/2.bf236c2a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.bf236c2a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3bf9b4b64fe2376c47d5",
+    "url": "/static/js/main.471a4545.chunk.js"
+  },
+  {
+    "revision": "b88fbb9f1ef718e933b9",
+    "url": "/static/js/runtime-main.507ac1cc.js"
+  },
+  {
+    "revision": "70065c31b4ff73a69e0b7350543ea7f1",
+    "url": "/static/media/covid19.70065c31.png"
+  }
+]);
